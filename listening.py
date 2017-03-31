a05063067de0786835260482c188d3cab456ba77
durkaya <betuldurkaya@gmail.com>
2017-03-31 16:10:20 +0300

    Database connection established and raw tweet data filtered and saved in MongoDB as json format

--- listening.py
+++ listening.py
@@ -1,8 +1,15 @@
+from pymongo import MongoClient
 import tweepy
 import json
 
+connection = MongoClient('localhost', 27017)
+db = connection.StreamData
+db.tweets.ensure_index("id", unique=True, dropDups=True)
+collection = db.tweets
+
 
 class StreamListener(tweepy.StreamListener):
+
     def on_connect(self):
         print("Tweet streaming begin.")
 
@@ -12,22 +19,16 @@ class StreamListener(tweepy.StreamListener):
 
     def on_data(self, raw):
         data = json.loads(raw)
-        fs = open("tweets.txt", "a")
+        tweet_id = data['id_str']  # The ID of tweet from Twitter in string format
+        username = data['user']['screen_name']  # The Tweet author's username
+        text = data['text']  # The entire body of the Tweet
 
         try:
-            print(data, '\n\n\n\n')
-            print((data["user"]["screen_name"], data["text"]), '\n')
+            print(data, '\n', (data["user"]["screen_name"], data["text"]), '\n\n\n\n')
             if data["text"].find('RT @') is -1:
-                username = data["user_mentions"]["screen_name"]
-                str1 = 'RT @' + username + ': '
-                text = data["text"].strip(str1)
-                fs.write(username + ' said that ' + text + '\n\n\n')
-                print(username + ' said that ' + text + '\n\n\n')
-            else:
-                fs.write(data["user"]["screen_name"] + ' said that ' + data["text"] + ' \n\n\n')
-            fs.close()
+                tweet = {'id': tweet_id, 'username': username, 'text': text}
+                # Insert Tweet data to MongoDB
+                collection.save(tweet)
         except Exception as e:
-            print(e)
+                    print(e)
         return True
-
-
