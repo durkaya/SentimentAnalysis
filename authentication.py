9eae04af1caf4743f75be3dba59508691cd807a2
durkaya <betuldurkaya@gmail.com>
2017-04-03 14:05:17 +0300

    Comments are added.

--- authentication.py
+++ authentication.py
@@ -2,21 +2,26 @@ import tweepy
 from tweepy import OAuthHandler
 import listening
 
+#twitter api keys to authanticate
 consumer_key = "dtKTW35ISxLtPDCszilhNk6R2"
 consumer_secret = "pDoisuGvzTGYte5rNEtjguiixVyys0IrL98xu5tzxE7aG8KZCJ"
 
 access_token = "773843946-LBFza4JmUOrzrUOkahDdtHXi6AHwFQyjwH6j7Ibg"
 access_token_secret = "JU0ExxNVFkQbBzipW0EOL9Lkb6dm3RadGdfoJeaLdOUgm"
 
+#authentication sections
 auth = OAuthHandler(consumer_key, consumer_secret)
 print("First auth done")
 auth.set_access_token(access_token, access_token_secret)
 print("2nd auth done")
 
+#tracked word
 WORDS = ['iphone']
 
+#from listening library, StreamListener object created
 listener = listening.StreamListener(api=tweepy.API(wait_on_rate_limit=True))
+#from tweepy library, Stream begins
 streamer = tweepy.Stream(auth=auth, listener=listener)
 
 print("Tracking: " + str(WORDS))
-streamer.filter(track=WORDS, languages=["en"])
+streamer.filter(track=WORDS, languages=["en"]) #filtering keywords
--- listening.py
+++ listening.py
@@ -1,13 +1,15 @@
+
 from pymongo import MongoClient
 import tweepy
 import json
 
+#MongoDB connection is established and configured
 connection = MongoClient('localhost', 27017)
 db = connection.StreamData
-db.tweets.ensure_index("id", unique=True, dropDups=True)
+db.tweets.create_index("id", unique=True, dropDups=True)
 collection = db.tweets
 
-
+#organizing tweet information
 class StreamListener(tweepy.StreamListener):
 
     def on_connect(self):
@@ -24,9 +26,11 @@ class StreamListener(tweepy.StreamListener):
         text = data['text']  # The entire body of the Tweet
 
         try:
-            print('\n', (data["user"]["screen_name"], data["text"]), '\n\n\n\n')
-            if data["text"].find('RT @') is -1:
+            if data["text"].find('RT @') is -1: #if not exist
+                #Create object in json format
                 tweet = {'id': tweet_id, 'username': username, 'text': text}
+                #Pretty print
+                print(json.dumps(tweet, indent=4, sort_keys=True))
                 # Insert Tweet data to MongoDB
                 collection.save(tweet)
         except Exception as e:
