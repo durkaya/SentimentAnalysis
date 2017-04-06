3da965fd032e06d80b27a732fec9e601e8ef8e6a
durkaya <betuldurkaya@gmail.com>
2017-04-06 20:12:23 +0300

    The tweet data is saved in .txt file again.

--- listening.py
+++ listening.py
@@ -1,17 +1,9 @@
-
-from pymongo import MongoClient
 import tweepy
 import json
 
-# MongoDB connection is established and configured
-connection = MongoClient('localhost', 27017)
-db = connection.StreamData
-db.tweets.create_index("id", unique=True, dropDups=True)
-collection = db.tweets
 
 # organizing tweet information
 class StreamListener(tweepy.StreamListener):
-
     def on_connect(self):
         print("Tweet streaming begin.")
 
@@ -25,16 +17,14 @@ class StreamListener(tweepy.StreamListener):
         time = data['created_at']  # The time of creation of the tweet
         username = data['user']['screen_name']  # The Tweet author's username
         text = data['text']  # The entire body of the Tweet
-
+        fs = open("tweets.txt", "a")
 
         try:
-            if data["text"].find('RT @') is -1:  # if not exist
-                # Create object in json format
-                tweet = {'id': tweet_id, 'created_at': time, 'username': username, 'text': text}
-                # Pretty print
-                print(json.dumps(tweet, indent=4, sort_keys=True))
-                # Insert Tweet data to MongoDB
-                collection.save(tweet)
+            # insert tweet data to tweet.txt file if RT is not exist
+            if data['text'].find('RT @') is -1:
+                fs.write(tweet_id + '\n' + time + '\n' + username + '\n' + text + '\n\n\n')
+                print(tweet_id + '\n' + time + '\n' + username + '\n' + text + '\n\n\n')
+            fs.close()
         except Exception as e:
-                    print(e)
+            print(e)
         return True
